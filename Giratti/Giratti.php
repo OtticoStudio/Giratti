430e8cd0556051812224045c5ea80d04c0a7e530
sabinrene <52389439+sabinrene@users.noreply.github.com>
2021-12-23 17:14:18 -0500

    OtticoUp
    ...

---
+++ Giratti/Giratti.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Giratti </title>
+
+
+	<link rel="stylesheet" href="../CSS/Ottico.css">
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+	</style>
+
+
+</head>
+<body>
+
+	<style type="text/css">
+		.gBanner {
+			position: relative;
+			text-align: center;
+			height: 20vh;
+			padding-top: 50px;
+		}
+	</style>
+
+	<section class="gBanner">
+		<h1> Giratti </h1>
+	</section>
+
+
+
+
+
+
+
+	<style type="text/css">
+		.Multi {
+			background: linear-gradient(to right, #00AAFF, #00FF6C);
+		}
+		.Multi ul {
+			padding: 20px;
+		}
+		.Multi ul h6 {
+			margin-bottom: 4px;
+		}
+		.Multi ul {
+			margin: 0px;
+			padding: 20px;
+		}
+		.Multi li {
+			font-size: 0.95em;
+			list-style: lower-norwegian;
+			margin-left: 20px;
+		}
+	</style>
+
+	<section class="Multi">
+		<h1> Multiplayer  </h1>
+		<h3> Real-time Interaction </h3>
+
+		<ul>
+			<h6> Photon </h6>
+			<li> <a href="https://www.photonengine.com/en-US/PUN"> PUN </a> </li>
+			<li> <a href="https://www.photonengine.com/en-US/BOLT"> BOLT </a></li>
+			<li> <a href="https://www.photonengine.com/en-US/Voice"> Voice Chat </a></li>
+			<li> <a href="https://www.photonengine.com/en-US/Bolt/Pricing"> Pricing </a></li>
+		</ul>
+
+		<ul>
+			<h6> Agora </h6>
+			<li> <a href="https://www.agora.io/en/blog/how-to-create-a-video-chat-app-in-unity/"> Agora Video Streaming </a> </li>
+			<li> <a href="https://www.agora.io/en/blog/how-to-broadcast-your-screen-with-unity3d-and-agora/ "> ScreenShare </a></li>
+			<li> <a href="https://www.agora.io/en/blog/implementing-spatial-audio-chat-in-unity-using-agora/"> Spatial Audio </a></li>
+		</ul>
+
+	</section>	
+
+
+
+	<style type="text/css">
+
+		.Owls {
+			position: relative;
+			display: flex;
+			flex-wrap: wrap;
+			flex-direction: column;
+
+			justify-content: center;
+			align-items: center;
+			height: 70vh;
+		}
+			.Owls img {
+				width: 150px;
+			}
+
+			.Fade30 { -webkit-mask-image: linear-gradient(to top, transparent 10%, black 30%); }
+			.Fade40 { -webkit-mask-image: linear-gradient(to top, transparent 10%, black 40%); }
+			.Fade50 { -webkit-mask-image: linear-gradient(to top, transparent 10%, black 50%); }
+			.Fade60 { -webkit-mask-image: linear-gradient(to top, transparent 10%, black 60%); }
+			.Fade70 { -webkit-mask-image: linear-gradient(to top, transparent 10%, black 70%); }
+
+		.Owls h4 {
+			margin-bottom: 40px;
+		}
+
+	</style>
+	
+	<section class="Owls">
+		<h1> #CSS </h1>
+		<h4> -Webkit-mask-image </h4>
+		<div class="Owl-Div">
+			<img class="Fade30" src="../images/Owl.png">
+			<img class="Fade40" src="../images/Owl.png">
+			<img class="Fade50" src="../images/Owl.png">
+			<img class="Fade60" src="../images/Owl.png">
+			<img class="Fade70" src="../images/Owl.png">
+		</div>
+
+	</section>
+
+
+
+
+	<style type="text/css">
+		.footer {
+			position: relative;
+			padding: 80px;
+			text-align: center;  
+			background: black;
+		}
+	</style>
+
+	<section class="footer">
+		<h2> Footer </h2>
+	</section>
+
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,368 @@
+<html>
+
+<head>
+	<title> Ottico Studio </title>	
+	
+	<link rel="stylesheet" href="CSS/Ottico.css">
+	<style type="text/css">
+		@import url('https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+		@import url('https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap');
+
+
+	</style>
+
+</head>
+
+
+<body>
+	
+	<!-- Menu -->
+	<style type="text/css">
+		.oMenu {
+			position: relative;
+			height: 10vh;
+		}
+	</style>
+	<section class="oMenu">
+		<h3> Menu </h3>
+	</section>
+
+
+
+
+
+	<!-- Ottico Banners -->
+	<style type="text/css">
+		.Banner-Sec {
+			position: relative;
+			height: 90vh;
+			background: linear-gradient(to right, #CE6EF0, #6FB0BE);
+		}
+			.Banner-Sec h1 {
+				font-size: 3.5em;
+			}
+
+		.Banner-Pic {
+			position: relative;
+			height: 90vh;
+
+			background: 
+				/* transparent gradient */ 
+			    linear-gradient(rgba(206, 110, 240, .75), rgba(111, 176, 190, .5)
+			    ), url("images/00-Home/Charles-Postiaux.jpg");
+			background-repeat: no-repeat;
+			width: 100%;
+			margin: 0 auto;
+			background-position: center;
+		}
+		.Banner-Txt {
+			position: relative;
+			text-align: center;
+			width: fit-content;
+			left: 50%;
+			top: 50%;
+			transform: translate(-50%, -50%);
+		}
+		.Banner-Txt p {
+			font-size: 0.85em;
+		}
+	</style>
+
+	<section class="Banner-Sec">
+		<div class="Banner-Pic">
+
+			<div class="Banner-Txt">
+				<h4> Digital Evolution </h4>
+				<h1> Ottico Studio </h1>
+				<p> SOFTWARE | WEB DESIGN | GAME DEV </p>
+			</div>
+
+		</div>
+	</section>
+	<!-- END Banners -->
+
+
+
+
+
+
+
+
+
+
+
+	<!-- Blank -->
+	<style type="text/css"> .Blank { height: 0.5vh; } </style>
+	<section class="Blank">  </section>
+
+
+	<!-- Giratti -->
+	<style type="text/css">
+		
+		.Giratti {
+			position: relative;
+			background: rgb(12,40,139);
+			background: linear-gradient(90deg, rgba(12,40,139,1) 9%, rgba(8,153,222,1) 86%);
+			padding: 20px;
+			height: 75vh;
+		}
+		.Giratti h4 {
+			color: white;
+			margin-bottom: 40px;
+		}
+		.Giratti a {
+			color: whitesmoke;
+		}
+	
+		.Giratti p {
+			width: 600px;
+			font-weight: 400;
+			color: var(--Wht);
+		}
+
+		.Giratti-Txt {
+			position: relative;
+			top: 50%;
+			left: 5%;
+			transform: translate(0%, -50%);
+			width: fit-content;
+		}
+
+		.Giratti-img {
+			position: absolute;
+			bottom: 0px;
+			right: 6%;
+		}
+		.Giratti-img img {
+			width: 95%;
+		}
+
+	</style>
+
+	<section class="Giratti">
+		
+		<div class="Giratti-Txt">
+			<h3> <a href="Giratti/Giratti.php"> Giratti </a> </h3>
+			<h4> Tuts & Courses </h4>
+			<p> Real-Time Tuts and Courses, developed for you & us by us. Enjoy Multi-Tier Courses to Explore your coding skills with a variety of fresh and popular, even obscure topics. Schedule your own time to learn from our course library today or tomorrow if you wish. Build better skills and better processes to improve workflows every where, any time.
+			</p>
+		</div>
+
+		<div class="Giratti-img">
+			<img src="images/00-Home/Giratti-Phones.png">
+		</div>
+
+	</section>
+
+
+
+
+
+	<style type="text/css">
+		.Demo {
+			position: relative;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			height: 55vh;
+
+		}
+		.DemoTxt {
+			position: relative; 
+			width: fit-content;
+			padding: 40px;
+		}
+		.Demo-Btns {
+			position: relative;
+			top: 15px;
+		}
+	</style>
+	<section class="Demo">
+		<div class="DemoTxt">
+			<h1> Request a Demo </h1>
+			<p> Call us to experience Ottico Fair on your own PC </p>
+
+			<div class="Demo-Btns">
+				<button class="Btn4"> Call Today </button>
+				<button class="Btn4"> Email Us </button>
+			</div>
+		</div>
+	</section>
+
+
+
+
+
+
+
+
+	<style type="text/css">
+
+		.CodeArea {
+			position: relative;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+
+			font-family: sans-serif;
+			background-image: linear-gradient(45deg, #fe6c5f 50%, #9b0123 100%);
+			height: 70vh;
+			color: white;
+		}
+
+		.CodeTxt {
+			height: fit-content;
+			padding: 2em 4em 2em 1em;
+			border-radius: 0.25em;
+			box-shadow: 0px 4px 10px #9b0123;
+			background-color: #1d1e22;
+		}
+
+			.CodeArea h3 {
+				color: white;
+				font-size: 4em;
+				margin-bottom: 20px;
+			}
+			.CodeArea p {
+				color: white;
+			}
+
+
+	</style>
+
+	<section class="CodeArea">
+		
+		<h3> #CODE </h3>
+		<div class="CodeTxt">
+			<p> 
+					html { <br/>
+						scroll-behavior: smooth; <br/> 
+					} 
+			</p>
+		</div>
+
+	</section>
+
+
+
+
+	<style type="text/css">
+		.GradText {
+			background: var(--DrkBg);
+			text-align: right;
+			padding-bottom: 1.5em;
+		}
+		.GradText a {
+			color: transparent;
+		}
+
+		.GradText h1 {
+			 font-size: 72px;
+			 background: -webkit-linear-gradient(-20deg, #00cdac 0%, #8ddad5 100%);
+			 -webkit-background-clip: text;
+			 -webkit-text-fill-color: transparent;
+		}
+
+		.GradTxt {
+			height: 40vh;
+		}
+
+	</style>
+	<section class="GradText ">
+		
+		<a href="https://cssgradient.io/blog/css-gradient-text/"> Tutorial </a>
+		<div class="GradTxt FlexCenter">
+		 <h1>Ottico Studio</h1>
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+
+
+	<!-- -------- -->
+	<!-- StoryDrk -->
+	<!-- -------- -->
+	<style type="text/css">
+		.StoryDrk {
+			position: relative;
+			height: auto;
+		}
+		.StoryPad {
+			padding: 2% 9% 8% 9%;
+		}
+
+		.cRed 		{ color: var(--redOnDrk); }
+		.cLightBlue { color: var(--LightBlueOnDrk); }
+		.cMustard 	{ color: var(--Mustard); }
+    }
+	
+	</style>
+	<section class="StoryDrk DrkMode">
+		
+		<div class="FlexRow">
+			<div class="Flex2Col-6535-L FlexCenter" style="background: black;">
+					<h6 class="Subtitle"> Left </h6>
+					<h1> <span class="cRed">65</span>%</h1>
+			</div>
+
+			<div class="Flex2Col-6535-R FlexCenter">
+					<h6 class="Subtitle"> Right </h6>
+					<h1> <span class="cRed">35</span>%</h1>
+			</div>
+
+		</div>
+
+
+		<div class="StoryPad">
+			<h6 class="Subtitle"> Game Development </h6>
+			<h1> <span class="cLightBlue">Simple</span> workflows fast delivery </h1>
+			<p> Build funnels, see top user flows, create cohorts, and more with just a few clicks. No need to invest hours writing SQL or waiting on someone to do it for you. </p>
+			<a> Try it with Ottico Analytics -> </a>
+		</div>
+
+	</section>
+
+
+	<style type="text/css">
+
+		.StoryParagrh {
+			position: relative;
+			height: auto;
+			width: 700px;
+			margin: 0 auto;
+			padding-bottom: 40px;
+			text-align: center;
+		}
+			.StoryParagrh h4 {
+				margin-bottom: 20px;
+			}
+	
+	</style>
+	<section class="StoryDrk DrkMode">
+		
+		<div class="StoryPad"> 
+			<h6 class="Subtitle"> Virtual Tours </h6>
+			<h1> <span class="cMustard">Explore</span> 360 environments </h1>
+			<p> Build funnels, see top user flows, create cohorts, and more with just a few clicks. No need to invest hours writing SQL or waiting on someone to do it for you. </p>
+			<a> Try it with Ottico Analytics -> </a>
+		</div>
+
+		<div class="StoryParagrh FlexCenter	">
+			<h4> <span class="cMustard">All the ways to get data into Mixpanel</span> </h4>
+			<p> In addition to loading data from your warehouse, you can stream events directly from your apps and servers using a customer data platform (CDP) or Mixpanel SDKs. Gandalf the great white wizard totally killed Voldemort in a parallel universe where those two would meet. </p>
+		</div>
+
+	</section>
+
+</body>
+
+</html>
